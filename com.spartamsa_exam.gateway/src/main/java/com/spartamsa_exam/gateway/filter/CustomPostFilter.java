ab4dfb93caf78320296cd68dc9a48977656e2290
sangwoo lee <lswoo0705@gmail.com>
2024-08-08 21:17:03 +0900

    Gateway Filter

---
+++ com.spartamsa_exam.gateway/src/main/java/com/spartamsa_exam/gateway/filter/CustomPostFilter.java
@@ -0,0 +1,29 @@
+package com.spartamsa_exam.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.logging.Logger;
+
+public class CustomPostFilter implements GlobalFilter, Ordered {
+    private static final Logger logger = Logger.getLogger(CustomPreFilter.class.getName());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            ServerHttpResponse response = exchange.getResponse();
+            logger.info("Post Filter : Response Status code is " + response.getStatusCode());
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
