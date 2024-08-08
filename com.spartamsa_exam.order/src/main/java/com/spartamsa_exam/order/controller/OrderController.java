69474acf6dfd99a9b76c1c7282793e0bd1504e5d
sangwoo lee <lswoo0705@gmail.com>
2024-08-08 20:00:20 +0900

    Server - Product - Order 연결

--- com.spartamsa_exam.order/src/main/java/com/spartamsa_exam/order/controller/OrderController.java
+++ com.spartamsa_exam.order/src/main/java/com/spartamsa_exam/order/controller/OrderController.java
@@ -1,4 +1,19 @@
 package com.spartamsa_exam.order.controller;
 
+import com.spartamsa_exam.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
+
+    // 주문 단건 조회
+    @GetMapping("/order/{orderId}")
+    public String getOrder(@PathVariable("orderId") String orderId) {
+        return orderService.getOrder(orderId);
+    }
 }
--- com.spartamsa_exam.order/src/main/java/com/spartamsa_exam/order/service/OrderService.java
+++ com.spartamsa_exam.order/src/main/java/com/spartamsa_exam/order/service/OrderService.java
@@ -1,4 +1,20 @@
 package com.spartamsa_exam.order.service;
 
+import com.spartamsa_exam.order.client.ProductClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+    private final ProductClient productClient;
+
+    public String getProductInfo(String productId) {
+        return productClient.getProduct(productId);
+    }
+
+    // 주문 단건 조회
+    public String getOrder(String orderId) {
+        return null;
+    }
 }
